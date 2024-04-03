a1220616809af9f05c31cd30ee260b57186cdfac
Jag_k <jag-k@users.noreply.github.com>
2024-04-03 22:45:59 +0400

    Fix style of formula
    Signed-off-by: Jag_k <jag-k@users.noreply.github.com>

--- Formula/git-raycast.rb
+++ Formula/git-raycast.rb
@@ -6,18 +6,14 @@ class GitRaycast < Formula
 
   homepage "https://github.com/#{REPO}"
   url "https://github.com/jag-k/git-raycast/releases/download/v0.0.1/git-raycast"
-  license "MIT"
   sha256 "11fd7783eeae2913ed11d508b4ea6491be18d820a300e7cdc4a2aa17c71f9022"
-
+  license "MIT"
   head "https://github.com/#{REPO}.git"
-  head do
-    depends_on 'go' => :build
-  end
+
+  depends_on "go" => :build
 
   def install
-    if build.head?
-      system "go", "build", "-o", NAME, "cmd/#{NAME}"
-    end
+    system "go", "build", "-o", NAME, "cmd/#{NAME}" if build?
     bin.install NAME
   end
 
