13427e96c4ac24cf56063ef0cca81aafbf8b0e94
Jag_k <jag-k@users.noreply.github.com>
2024-04-04 15:44:56 +0400

    Update git-raycast formula to include support for different CPU architectures
    Signed-off-by: Jag_k <jag-k@users.noreply.github.com>

--- Formula/git-raycast.rb
+++ Formula/git-raycast.rb
@@ -5,17 +5,24 @@ class GitRaycast < Formula
   desc "Automate git using Raycast AI"
 
   homepage "https://github.com/#{REPO}"
-  url "https://github.com/jag-k/git-raycast/releases/download/v0.0.2/git-raycast"
+  url "https://github.com/jag-k/git-raycast/releases/download/v0.0.2/git-raycast.tar.gz"
   sha256 "4b7040cdf6c1963d13b28803fb80a8a81b9cd22a9397d1c14f6d6ff35c1d6a7d"
   license "MIT"
-  head "https://github.com/#{REPO}.git"
 
-  depends_on "go" => :build
+  head do
+    url "https://github.com/#{REPO}.git", branch: "main"
+    depends_on "go" => :build
+  end 
 
   def install
-    system "go", "build", "-o", NAME, "cmd/#{NAME}" if build.head?
+    if build.head?
+      system "go", "build", "-o", NAME, "cmd/#{NAME}"
+      bin.install NAME
+    else
+      bin.install "#{name}_amd64" => NAME if Hardware::CPU.intel?
+      bin.install "#{name}_arm64" => NAME if Hardware::CPU.arm?
+    end
     system "#{bin}/#{NAME}", "--create-man-page", "man.1"
-    bin.install NAME
     man1.install "man.1"
   end
 
