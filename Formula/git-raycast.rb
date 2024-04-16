230148d15adb948a2a10429b6b1177a22684a174
Jag_k <30597878+jag-k@users.noreply.github.com>
2024-04-16 15:58:47 +0400

    Update git-raycast to v0.1.0
    Signed-off-by: Jag_k <jag-k@users.noreply.github.com>

--- Formula/git-raycast.rb
+++ Formula/git-raycast.rb
@@ -5,8 +5,8 @@ class GitRaycast < Formula
   desc "Automate git using Raycast AI"
 
   homepage "https://github.com/#{REPO}"
-  url "https://github.com/jag-k/git-raycast/releases/download/v0.0.2/git-raycast.tar.gz"
-  sha256 "1b774feb0fb08115670d1c125ea857769b2b7d5f114fac15dc8cad997dd43e9a"
+  url "https://github.com/jag-k/git-raycast/releases/download/v0.1.0/git-raycast.tar.gz"
+  sha256 "ef1fb497315aefb28ec3ba4b6c6cce8b8454bce76c93f2c02ddabe462855e83a"
   license "MIT"
 
   head do
@@ -22,8 +22,8 @@ class GitRaycast < Formula
       bin.install "#{name}_amd64" => NAME if Hardware::CPU.intel?
       bin.install "#{name}_arm64" => NAME if Hardware::CPU.arm?
     end
-    system "#{bin}/#{NAME}", "--create-man-page", "#{NAME}.1"
-    man1.install "#{NAME}.1"
+    bash_completion.install "autocomplete/#{NAME}.bash" => "#{NAME}.bash_completion"
+    zsh_completion.install "autocomplete/#{NAME}.zsh" => "_#{NAME}"
   end
 
   test do
